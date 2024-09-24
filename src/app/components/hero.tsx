e5dd84aef04501ffe00058d91f0e9c92fdb4d119
sai karthik mudigonda <saikarthik.mudigonda@techwave.net>
2024-09-24 16:59:43 +0530

    css change

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -13,7 +13,7 @@ const AntonFont =  Anton({
 const Hero = () => {
   return (
     <>
-      <div className="p-2 ">
+      <div className="p-2 relative">
 
         <Image
           src="https://images.unsplash.com/photo-1603189343302-e603f7add05a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -22,9 +22,9 @@ const Hero = () => {
           alt="hero"
         ></Image>
         {/* <Image src="https://images.unsplash.com/photo-1638959882980-906e24520fe5?q=80&w=2013&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" height={20} width={1620} alt="hero"></Image> */}
-        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 } }>
+        <motion.div initial={{ opacity: 0.4 }} whileInView={{ opacity: 1 } }>
           <div className={`${AntonFont.className}`}>
-            <h1 className="text-center intro-text text-purple-600">24°</h1>
+            <h1 className="text-center intro-text text-purple-600 absolute ">24°</h1>
           </div>
         </motion.div>
       </div>
